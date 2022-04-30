cb11ebf526584f0acec5e8884bf012dc844b526f
RyazanovE <sidewipe@yandex.ru>
2022-04-30 14:24:45 +0300

    sad

--- src/Accordeon.jsx
+++ src/Accordeon.jsx
@@ -12,16 +12,11 @@ const Accordeon = ({ title, children }) => {
       <button
         onClick={clickHandler}
         className={
-<<<<<<< HEAD
+
           " z-10 py-[10px] bg-slate-400 hover:bg-slate-300 transition-color duration-100 text-[18px] text-white hover:text-slate-400"
         }
       >
-=======
-          " justify-center flex items-center relative active:bg-slate-400 active:text-slate-300 z-10 py-[10px] bg-slate-400 hover:bg-slate-300 transition-color duration-100 text-[18px] text-white hover:text-slate-400"
-        }
-      >
         
->>>>>>> 7c793a304f3a52cd072a2e9d34ad5a6ad8ee9e0d
         {title}
         <svg
           className={(isActive ? "rotate-180" : "rotate-0") + " fill-white transition-transform duration-100 ease-in-out w-[30px] h-[30px] absolute right-[20px] top-[50%-15px]"}
@@ -31,10 +26,6 @@ const Accordeon = ({ title, children }) => {
           <path d="M352 352c-8.188 0-16.38-3.125-22.62-9.375L192 205.3l-137.4 137.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25C368.4 348.9 360.2 352 352 352z" />
         </svg>
       </button>
-<<<<<<< HEAD
-=======
-
->>>>>>> 7c793a304f3a52cd072a2e9d34ad5a6ad8ee9e0d
       <div
         className={
           (isActive ? "max-h-[150px]" : "max-h-0") +
